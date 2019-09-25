139ea7a60550d2bd2d4d2553eb4870e8e856239d
elalaouifaris <elalaouifaris@gmail.com>
2019-09-25 19:56:52 +0200

    Add authentication to controller test

--- test/orga_app_web/controllers/page_controller_test.exs
+++ test/orga_app_web/controllers/page_controller_test.exs
@@ -1,6 +1,15 @@
 defmodule OrgaAppWeb.PageControllerTest do
   use OrgaAppWeb.ConnCase
 
+  alias OrgaApp.Users.User
+  
+  setup %{conn: conn} do
+    user = %User{email: "test@example.com"}
+    conn = Pow.Plug.assign_current_user(conn, user, otp_app: :orga_app)
+
+    {:ok, conn: conn}
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
