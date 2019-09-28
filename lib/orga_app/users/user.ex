e6fdc128eabca3d1c34ace958095b77242a19386
elalaouifaris <elalaouifaris@gmail.com>
2019-09-28 18:49:00 +0200

    Implement invitation workflow

--- lib/orga_app/users/user.ex
+++ lib/orga_app/users/user.ex
@@ -1,6 +1,8 @@
 defmodule OrgaApp.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
+  use Pow.Extension.Ecto.Schema,
+    extensions: [PowResetPassword, PowEmailConfirmation, PowInvitation]
 
   alias OrgaApp.Users.User
 
@@ -15,7 +17,17 @@ defmodule OrgaApp.Users.User do
   def changeset(user_or_changeset, attrs) do
     user_or_changeset
     |> pow_changeset(attrs)
-    # |> pow_extension_changeset(attrs)
+    |> pow_extension_changeset(attrs)
     |> Ecto.Changeset.assoc_constraint(:organization)
+  end
+
+   def invite_changeset(user_or_changeset, invited_by, attrs) do
+    user_or_changeset
+    |> pow_invite_changeset(invited_by, attrs)
+    |> changeset_organization(invited_by)
+  end
+
+  defp changeset_organization(changeset, invited_by) do
+    Ecto.Changeset.change(changeset, organization_id: invited_by.organization_id)
   end
 end
